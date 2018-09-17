04495412c3e8c26a353cb35841154302254e1826
Mikx0125 <31879838+Mikx0125@users.noreply.github.com>
2018-09-17 12:27:17 +0200

    n

--- Math.cs
+++ Math.cs
@@ -15,14 +15,23 @@ namespace MyMathDLL
         {
             return a + b;
         }
+        /// <summary>
+        /// Mulitple A and B
+        /// </summary>
         public int Multiple (int a, int b)
         {
             return a * b;
         }
+        /// <summary>
+        /// /Substract a and B
+        /// </summary>
         public int Substract (int a, int b)
         {
             return a - b;
         }
+        /// <summary>
+        /// Divide bro
+        /// </summary>
         public int Divide (int a, int b)
         {
             return a / b;
