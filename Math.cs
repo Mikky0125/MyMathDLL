505690e8714a52865dd35c7c21372c4ab32ac769
Mikx0125 <31879838+Mikx0125@users.noreply.github.com>
2018-09-17 10:52:09 +0200

    Initial commit

---
+++ Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathDLL
+{
+    public class MyMath
+    {
+        /// <summary>
+        /// Int a + b
+        /// </summary>
+        public int Add (int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiple (int a, int b)
+        {
+            return a * b;
+        }
+        public int Substract (int a, int b)
+        {
+            return a - b;
+        }
+        public int Divide (int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
